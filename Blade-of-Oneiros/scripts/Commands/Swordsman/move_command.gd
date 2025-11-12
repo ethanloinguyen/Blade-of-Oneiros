8322da5fd88ca4e600a92a9d4eaf62d0e50ded72
Jerome Hernandez <jmhern@ucdavis.edu>
2025-11-12 14:33:08 -0800

    Fix running velocity and add death/hurt animation to sprite

--- Blade-of-Oneiros/scripts/Commands/Swordsman/move_command.gd
+++ Blade-of-Oneiros/scripts/Commands/Swordsman/move_command.gd
@@ -3,11 +3,6 @@ extends Command
 
 
 func execute(character: Character) -> Status:
-	if Input.is_action_pressed("run"):
-		character.running = true
-	else:
-		character.running = false
-	
 	if Input.is_action_pressed("ui_up"):
 		character.change_facing(Character.Facing.UP)
 	if Input.is_action_pressed("ui_down"):
@@ -17,11 +12,18 @@ func execute(character: Character) -> Status:
 	if Input.is_action_pressed("ui_right"):
 		character.change_facing(Character.Facing.RIGHT)
 	
-	character.velocity.x += character.direction.x * character.move_speed
-	character.velocity.y += character.direction.y * character.move_speed
+	if Input.is_action_pressed("run"):
+		character.running = true
+	else:
+		character.running = false
 	
 	if character.running:
+		character.velocity.x += character.direction.x * character.run_speed
+		character.velocity.y += character.direction.y * character.run_speed
 		character.play_animation("run")
 	else:
+		character.velocity.x += character.direction.x * character.move_speed
+		character.velocity.y += character.direction.y * character.move_speed
 		character.play_animation("walk")
+	
 	return Status.DONE
