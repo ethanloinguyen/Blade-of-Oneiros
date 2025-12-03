8090f35f70240516428f1e086df27a0a9c4d8f05
Jerome Hernandez <jmhern@ucdavis.edu>
2025-12-03 02:23:19 -0800

    Add animation tree/player (too fast)

--- Blade-of-Oneiros/scripts/Commands/Swordsman/move_command.gd
+++ Blade-of-Oneiros/scripts/Commands/Swordsman/move_command.gd
@@ -12,18 +12,11 @@ func execute(character: Character) -> Status:
 	if Input.is_action_pressed("ui_right"):
 		character.change_facing(Character.Facing.RIGHT)
 	
-	if Input.is_action_pressed("run"):
-		character.running = true
-	else:
-		character.running = false
-	
 	if character.running:
 		character.velocity.x += character.direction.x * character.run_speed
 		character.velocity.y += character.direction.y * character.run_speed
-		character.play_animation("run")
 	else:
 		character.velocity.x += character.direction.x * character.move_speed
 		character.velocity.y += character.direction.y * character.move_speed
-		character.play_animation("walk")
 	
 	return Status.DONE
