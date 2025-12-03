40f3b1a065d25731eab3fbe56b594246ccaf4f2e
Qtypes <ngyn.quinton@gmail.com>
2025-12-03 04:56:03 -0800

    Manually Merged Player and Swordsman scenes/scripts from Jeromes Branch
    Finished working implementation of the pushable boxes and manually combined the swordsman scenes/ scripts of Jerome's branch.
    
    Box notes: Because of the intended behavior of the pushable boxes, rigidbody2d did not give the intended result. Switching to characterbody2d and instead relying on raycasting to detect collision/ push the box  rather than a push area with rigidbody2d, fixed the behavior. As the boxes are to be used for puzzles, I needed them to be strict to the grid and not too free form/moving. This helps future box puzzle/level designs.

--- Blade-of-Oneiros/scripts/interactable_scripts/pushable_box.gd
+++ Blade-of-Oneiros/scripts/interactable_scripts/pushable_box.gd
@@ -1,20 +1,62 @@
 class_name PushableBox
-extends RigidBody2D
+extends CharacterBody2D
+
+@export var gap: float = 1.0              
+@export var move_speed: float = 60.0      
+@onready var collision_shape: CollisionShape2D = $CollisionShape2D
+var is_moving: bool = false
+var box_step: Vector2 = Vector2.ZERO
+var target_pos: Vector2 = Vector2.ZERO
 
 
-@export var push_speed : float = 30.0
-var push_direction : Vector2 = Vector2.ZERO
-
-@onready var audio : AudioStreamPlayer2D = $AudioStreamPlayer2D
-
-
-func _physics_process(_delta: float) -> void:
-	if push_direction == Vector2.ZERO:
-		linear_velocity = Vector2.ZERO	
-	else:
-		linear_velocity = push_direction * push_speed
-		
-		
-func _set_push(value: Vector2) -> void:
-	push_direction = value
+func _ready() -> void:
+	#We want to use the box's collision shape as the grid/step size, to make puzzle pushes strict
+	var rect_shape: RectangleShape2D = collision_shape.shape as RectangleShape2D
+	var size: Vector2 = rect_shape.size * global_scale
 	
+	box_step = size + Vector2(gap, gap)
+
+	#Snap starting position to the box grid
+	var snapped_x: float = round(global_position.x / box_step.x) * box_step.x
+	var snapped_y: float = round(global_position.y / box_step.y) * box_step.y
+	global_position = Vector2(snapped_x, snapped_y)
+	target_pos = global_position
+
+
+func _physics_process(delta: float) -> void:
+	if is_moving:
+		var to_target: Vector2 = target_pos - global_position
+		var distance: float = to_target.length()
+
+		if distance <= move_speed * delta:
+			global_position = target_pos
+			is_moving = false
+
+		else:
+			var direction: Vector2 = to_target / distance
+			var motion: Vector2 = direction * move_speed * delta
+			move_and_collide(motion)
+
+
+func push(raw_dir: Vector2) -> bool:
+	if is_moving:
+		return false
+
+	var direction: Vector2 = DirectionSnap._snap_to_cardinal(raw_dir)
+	if direction == Vector2.ZERO:
+		return false
+
+	var motion: Vector2
+	if direction.x != 0.0:
+		motion = Vector2(sign(direction.x) * box_step.x, 0.0)
+	else:
+		motion = Vector2(0.0, sign(direction.y) * box_step.y)
+
+	var from_transform: Transform2D = global_transform
+
+	if test_move(from_transform, motion):
+		return false
+
+	target_pos = global_position + motion
+	is_moving = true
+	return true
--- Blade-of-Oneiros/scripts/player.gd
+++ Blade-of-Oneiros/scripts/player.gd
@@ -1,23 +1,47 @@
 class_name Player
 extends Character
 
+@export var health:int = 100
+
+@onready var animation_tree: AnimationTree = $AnimationTree
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var push_ray: RayCast2D = $PushRay
+
+var _damaged: bool = false
+var _dead: bool = false
+
 var move_cmd: Command
 var attack_cmd: Command
 var idle_cmd: Command
+var facing_direction: Vector2 = Vector2.DOWN
+
 
 func _ready() -> void:
+	animation_tree.active = true
+	animation_player.speed_scale = 0.1
 	bind_commands()
 
+
 func _physics_process(delta: float) -> void:
+	if _dead:
+		return
+	
 	# if player isn't current attacking and pressed attack, then attack
 	if attacking:
 		super(delta)
+		_manage_animation_tree_state()
 		return
 	
 	if Input.is_action_just_pressed("attack"):
 		attack_cmd.execute(self)
+		_manage_animation_tree_state()
 		return
-		
+	
+	if Input.is_action_pressed("run"):
+		running = true
+	else:
+		running = false
+	
 	# Get player input direction
 	direction = Vector2(
 		Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"),
@@ -28,6 +52,21 @@ func _physics_process(delta: float) -> void:
 	if direction.length() > 1:
 		direction = direction.normalized()
 	
+	#Ray updates in frame if while pressing directional input against box,
+	# the ray collides with the box, it will continuously call push in that direction
+	if direction != Vector2.ZERO:
+		facing_direction = DirectionSnap._snap_to_cardinal(direction)
+	
+	if push_ray != null:
+		var ray_length: float = 8
+		push_ray.target_position = facing_direction * ray_length
+		push_ray.force_raycast_update()
+	
+		if direction != Vector2.ZERO and push_ray.is_colliding():
+			var collider_object: Object = push_ray.get_collider()
+			if collider_object is PushableBox:
+				var box: PushableBox = collider_object as PushableBox
+				box.push(facing_direction)
 	# Reset velocity every frame
 	velocity = Vector2.ZERO
 	
@@ -42,6 +81,20 @@ func _physics_process(delta: float) -> void:
 	
 	_manage_animation_tree_state()
 
+
+func take_damage(damage: int) -> void:
+	health -= damage
+	_damaged = true
+	if health <= 0:
+		# play death audio here
+		_dead = true
+		animation_tree.active = false
+		animation_tree.play("death")
+	else:
+		# play hurt audio here
+		pass
+
+
 func bind_commands() -> void:
 	move_cmd = MoveCommand.new()
 	attack_cmd = AttackCommand.new()
@@ -49,4 +102,36 @@ func bind_commands() -> void:
 
 
 func _manage_animation_tree_state() -> void:
-	pass
+	# Always update directional blend spaces
+	if (direction != Vector2.ZERO):
+		animation_tree["parameters/idle/blend_position"] = direction
+		animation_tree["parameters/walk/blend_position"] = direction
+		animation_tree["parameters/run/blend_position"] = direction
+		animation_tree["parameters/attack/blend_position"] = direction
+		animation_tree["parameters/hurt/blend_position"] = direction
+		animation_tree["parameters/death/blend_position"] = direction
+	
+	if (velocity == Vector2.ZERO):
+		animation_tree["parameters/conditions/idle"] = true
+		animation_tree["parameters/conditions/moving"] = false
+	else:
+		animation_tree["parameters/conditions/idle"] = false
+		animation_tree["parameters/conditions/moving"] = true
+	
+	# Toggles
+	if attacking:
+		animation_tree["parameters/conditions/attacking"] = true
+		attacking = false
+	else:
+		animation_tree["parameters/conditions/attacking"] = false
+		
+	if _damaged:
+		animation_tree["parameters/conditions/damaged"] = true
+		_damaged = false
+	else:
+		animation_tree["parameters/conditions/damaged"] = false
+		
+	if running:
+		animation_tree["parameters/conditions/running"] = true
+	else:
+		animation_tree["parameters/conditions/running"] = false
