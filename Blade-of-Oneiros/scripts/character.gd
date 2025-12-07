6f7b3cd2b48d155037106d87b852c984d4ecd29c
Jerome Hernandez <jmhern@ucdavis.edu>
2025-12-06 22:21:37 -0800

    Add stamina bar and exhaustion

--- Blade-of-Oneiros/scripts/character.gd
+++ Blade-of-Oneiros/scripts/character.gd
@@ -29,18 +29,3 @@ func change_facing(new_facing: Facing) -> void:
 	if facing != new_facing:
 		facing = new_facing
 		emit_signal("direction_changed", facing)
-
-## Command helper for consistent animation naming.
-#func play_animation(state: String) -> void:
-	#var facing_name: String
-	#match facing:
-		#Facing.UP: facing_name = "up"
-		#Facing.DOWN: facing_name = "down"
-		#Facing.LEFT: facing_name = "left"
-		#Facing.RIGHT: facing_name = "right"
-		#_: facing_name = "down"
-	#
-	#var anim_name = "%s_%s" % [state, facing_name]  # e.g. walk_down
-	#if anim_sprite.animation != anim_name:
-		#anim_sprite.play(anim_name)
-	#current_state = state
