7cc877626736be710bce01e2e5b4112398694ffd
bnelson1324 <bnelson1324>
2025-12-07 10:47:44 -0800

    boss2 stages

--- Blade-of-Oneiros/scripts/enemy_ai/boss/boss_2.gd
+++ Blade-of-Oneiros/scripts/enemy_ai/boss/boss_2.gd
@@ -11,15 +11,15 @@ extends CharacterBody2D
 @export var projectile:PackedScene
 
 @export var next_boss:PackedScene
-@export var slime_enemy:PackedScene
-@export var death_slime_spawn_count:int = 3
 
 var fsm:FSM
 var idle_state:State
-var jump_state:JumpState
-var shoot_state:ShootState
-var attack_state:State
-var stun_state:State
+var jump_state_1:JumpState
+var shoot_state_1:ShootState
+var jump_state_2:JumpState
+var shoot_state_2:ShootState
+var jump_state_3:JumpState
+var shoot_state_3:ShootState
 
 var _player:Player
 var _dir:String = "down"
@@ -48,12 +48,6 @@ func _ready():
 			var boss:CharacterBody2D = next_boss.instantiate()
 			get_parent().add_child(boss)
 			boss.global_position = global_position
-		for i in range(death_slime_spawn_count):
-			var s:CharacterBody2D = slime_enemy.instantiate()
-			get_parent().add_child(s)
-			var a = float(i)/death_slime_spawn_count * TAU
-			var offset = Vector2(cos(a), sin(a))
-			s.global_position = global_position + offset * 20
 
 		queue_free()
 	)
@@ -70,20 +64,40 @@ func _ready():
 		func():
 		_face_player()
 	)
-	jump_state = JumpState.new(self, 100, 1.0, sprite, attack_hitbox, func():
-		_idle(between_states_wait_duration, func():
-			shoot_state.shoot(global_position)
+	jump_state_1 = JumpState.new(self, 100, 1.0, sprite, attack_hitbox, func():
+		_idle(0.1, func():
+			shoot_state_1.shoot(global_position)
 		)
 	)
-	shoot_state = ShootState.new(self, projectile, 4, false, func():
+	shoot_state_1 = ShootState.new(self, projectile, 4, false, func():
 		_idle(between_states_wait_duration, func():
-			jump_state.jump(_player.global_position)
+			jump_state_2.jump(_player.global_position)
+		)
+	)
+	jump_state_2 = JumpState.new(self, 100, 1.0, sprite, attack_hitbox, func():
+		_idle(0.1, func():
+			shoot_state_2.shoot(global_position)
+		)
+	)
+	shoot_state_2 = ShootState.new(self, projectile, 4, true, func():
+		_idle(between_states_wait_duration, func():
+			jump_state_3.jump(_player.global_position)
+		)
+	)
+	jump_state_3 = JumpState.new(self, 100, 1.0, sprite, attack_hitbox, func():
+		_idle(0.1, func():
+			shoot_state_3.shoot(global_position)
+		)
+	)
+	shoot_state_3 = ShootState.new(self, projectile, 4, false, func():
+		_idle(4.0, func():
+			jump_state_1.jump(_player.global_position)
 		)
 	)
 	fsm = FSM.new(idle_state)
 
 	await get_tree().process_frame
-	jump_state.jump(_player.global_position)
+	jump_state_1.jump(_player.global_position)
 
 
 func _physics_process(delta:float) -> void:
