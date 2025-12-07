4ee28109782cc80387aff26eff8de4cdeb5836cf
Ethan Nguyen <etanguyen@ucdavis.edu>
2025-12-07 05:18:29 -0800

    Video connected to button

--- Blade-of-Oneiros/scripts/title_screen/title_screen.gd
+++ Blade-of-Oneiros/scripts/title_screen/title_screen.gd
@@ -1,11 +1,17 @@
 extends Node
 
-@onready var start_button: TextureButton = $StartButton
+@onready var start_button = $CanvasLayer/StartButton
+@onready var intro_video = $CanvasLayer/IntroVideo
+@onready var background = $CanvasLayer/Background
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	start_button.pressed.connect(_on_start_button_pressed)
+	intro_video.finished.connect(_on_video_finished)
+	
+	#Hide video until button is pressed
+	intro_video.visible = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -13,7 +19,18 @@ func _process(delta: float) -> void:
 	pass
 	
 func _on_start_button_pressed():
-	GameState.game_started = true
-	$Background.visible = false
+	background.visible = false
+	start_button.visible = false
 	
+	#Show and start video
+	intro_video.visible = true
+	intro_video.play()
+	
+func _on_video_finished():
+	GameState.game_started = true
+	get_tree().change_scene_to_file("res://scenes/level_scenes/lvl_1.tscn")
+	
+	
+	
+
 		
