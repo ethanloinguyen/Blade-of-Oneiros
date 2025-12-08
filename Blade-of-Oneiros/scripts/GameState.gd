ed55b6aea9c8b7961ec36c11082ffec0ed0f229c
Jerome Hernandez <jmhern@ucdavis.edu>
2025-12-08 02:53:13 -0800

    remove redundancy

--- Blade-of-Oneiros/scripts/GameState.gd
+++ Blade-of-Oneiros/scripts/GameState.gd
@@ -1,17 +1,8 @@
 extends Node
 
-
 var game_started := false
 var input_locked = false
 var game_over := false
 var last_scene_path: String = ""
 var last_spawn_tag: StringName = "default"
 var is_respawning: bool = false
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	pass
