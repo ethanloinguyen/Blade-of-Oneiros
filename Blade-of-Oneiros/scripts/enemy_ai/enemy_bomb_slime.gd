4a6e14f9e7c06ce312a0334e99f8c7acfd8c5d37
bnelson1324 <33214860+bnelson1324@users.noreply.github.com>
2025-12-10 00:45:58 -0800

    start adding enemy bomb slime

--- Blade-of-Oneiros/scripts/enemy_ai/enemy_bomb_slime.gd
+++ Blade-of-Oneiros/scripts/enemy_ai/enemy_bomb_slime.gd
@@ -1,10 +1,9 @@
-class_name Enemy
+class_name BombSlime
 extends CharacterBody2D
 
 @export var activate_distance:float
 @export var speed:float
 @export var move_avoid_collision_dist:float
-@export var chase_leash_distance:float
 
 @export var attack_distance:float
 
@@ -59,7 +58,7 @@ func _ready():
 	sprite.frame_changed.connect(func():
 		if sprite.animation.begins_with("death") and sprite.frame == 1:
 			play_audio(death_audio)
-		
+
 		if (sprite.animation.begins_with("hurt")):
 			play_audio(hurt_audio[randi() % hurt_audio.size()])
 	)
@@ -69,81 +68,78 @@ func _ready():
 		"Wait",
 		Callable(),
 		func(_delta:float):
-		# wait until player variable is set
-		if _player == null:
-			return
+			# wait until player variable is set
+			if _player == null:
+				return
 
-		AiHelper.update_velocity_walk(self, Vector2.ZERO, [], 32)
-		AiHelper.play_animation(sprite, "idle", _dir)
+			AiHelper.update_velocity_walk(self, Vector2.ZERO, [], 32)
+			AiHelper.play_animation(sprite, "idle", _dir)
 
-		# transition
-		if _player.global_position.distance_to(global_position) < activate_distance:
-			fsm.change_state(chase_state)
-		,
+			# transition
+			if _player.global_position.distance_to(global_position) < activate_distance:
+				fsm.change_state(chase_state)
+			,
 		Callable(),
 	)
 	chase_state = State.new(
 		"Chase",
 		Callable(),
 		func(_delta:float):
-		var dist_to_player:float = _player.global_position.distance_to(global_position)
-		var to_player:Vector2 = _player.global_position - global_position
-		var move_avoid_dirs:Array[Vector2] = []
-		if chase_leash_distance > dist_to_player:
-			move_avoid_dirs.push_back(to_player)
-		AiHelper.update_velocity_walk(self, to_player, move_avoid_dirs, 32)
+			var dist_to_player:float = _player.global_position.distance_to(global_position)
+			var to_player:Vector2 = _player.global_position - global_position
+			AiHelper.update_velocity_walk(self, to_player, [], 32)
 
-		# play animation
-		_dir = AiHelper.update_dir(to_player)
-		if velocity.is_zero_approx():
-			AiHelper.play_animation(sprite, "idle", _dir)
-		else:
-			AiHelper.play_animation(sprite, "walk", _dir)
+			# play animation
+			_dir = AiHelper.update_dir(to_player)
+			if velocity.is_zero_approx():
+				AiHelper.play_animation(sprite, "idle", _dir)
+			else:
+				AiHelper.play_animation(sprite, "walk", _dir)
 
-		# transition
-		if dist_to_player < attack_distance:
-			fsm.change_state(attack_state)
-		,
+			# transition
+			if dist_to_player < attack_distance:
+				fsm.change_state(attack_state)
+			,
 		Callable(),
 	)
 	attack_state = State.new(
-		"Attack",
-		func():
+	"Attack",
+	func():
 		AiHelper.play_animation(sprite, "attack", _dir)
 		#play_audio(slime_attack_audio)
 
 		# wait for attack animation to finish
 		await sprite.animation_finished
-		
+
 		if not is_instance_valid(self):
 			return
 		if fsm.current_state == attack_state:
 			fsm.change_state(chase_state)
 		,
-		func(_delta:float):
+	func(_delta:float):
 		velocity = Vector2.ZERO
-		,
-		Callable()
-	)
+	,
+	Callable()
+		)
 	stun_state = State.new(
 		"Stun",
 		func():
-		sprite.stop()
-		AiHelper.play_animation(sprite, "hurt", _dir)
-		_stun_knockback_dir = global_position - _player.global_position
-		_stun_timer = 0
-		await sprite.animation_finished
-		if not is_instance_valid(self):
-			return
-		fsm.change_state(chase_state)
-		,
+			sprite.stop()
+			AiHelper.play_animation(sprite, "hurt", _dir)
+			_stun_knockback_dir = global_position - _player.global_position
+			_stun_timer = 0
+			await sprite.animation_finished
+			if not is_instance_valid(self):
+				return
+			fsm.change_state(chase_state)
+			,
 		func(delta):
-		_stun_timer += delta
-		if _stun_timer < 0.2:
-			velocity = _stun_knockback_dir.normalized() * STUN_SPEED
-		else:
-			velocity = Vector2.ZERO
-		,
+			_stun_timer += delta
+			if _stun_timer < 0.2:
+				velocity = _stun_knockback_dir.normalized() * STUN_SPEED
+			else:
+				velocity = Vector2.ZERO
+			,
 		Callable()
 	)
 	fsm = FSM.new(wait_state)
@@ -153,7 +149,7 @@ func _physics_process(delta:float) -> void:
 	fsm.update(delta)
 	move_and_slide()
 
-	
+
 func play_audio(_stream : AudioStream) -> void:
 	audio.stream = _stream
 	audio.play()
