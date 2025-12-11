bc5f427fa20788650465419052488813e4a1568b
Qtypes <ngyn.quinton@gmail.com>
2025-12-10 21:23:52 -0800

    changed box tooltip message

--- Blade-of-Oneiros/scripts/dialogue/scripts/tooltips/dialogue_script_tooltip_fireplace.gd
+++ Blade-of-Oneiros/scripts/dialogue/scripts/tooltips/dialogue_script_tooltip_fireplace.gd
@@ -2,7 +2,7 @@ extends Resource
 
 func run(orchestrator):
 	orchestrator.narrate(
-		"I think I see a button in here...",
+		"This fire is cozy, maybe I should reset here...",
 		{
 			"color": Color(0.0, 0.0, 0.0, 1.0),
 			"size": 15,
